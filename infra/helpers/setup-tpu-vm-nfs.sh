2aaa4de6f06fbdd30f916f994e2f38ae0bdf5bb8
Javier de la Rosa <versae@gmail.com>
2024-04-23 17:52:36 +0200

    Update setup-tpu-vm-nfs.sh

--- infra/helpers/setup-tpu-vm-nfs.sh
+++ infra/helpers/setup-tpu-vm-nfs.sh
@@ -76,6 +76,14 @@ if [ "$CURRENT_NFS_ENTRY" != "$DESIRED_NFS_ENTRY" ]; then
 fi
 sudo mount -a
 
+# Install GitHub client CLI
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
 for x in `ls -d /share/lev*`; do
   git config --global --add safe.directory $x
 done
